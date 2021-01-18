b6c9d56f03ded96991590aa2593309f8bf757ca6
the-robot <9334746+the-robot@users.noreply.github.com>
2021-01-18 17:30:49 +0000

    feat: added solutions

--- leet-code/interviews.school/05-sets-and-maps/group-anagrams.cpp
+++ leet-code/interviews.school/05-sets-and-maps/group-anagrams.cpp
@@ -1,5 +1,7 @@
 // https://leetcode.com/problems/group-anagrams/
 
+#include <algorithm>
+#include <map>
 #include <string>
 #include <vector>
 
@@ -9,8 +11,31 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> result;
-        
-        
+        map<string, vector<string>> anagrams;
+
+        for (string str : strs) {
+            // sort the string to find as key
+            string sorted = str;
+            sort(sorted.begin(), sorted.end());
+
+            // find pos in the map
+            auto pos = anagrams.find(sorted);
+
+            // create new bucket if not exists
+            if (pos == anagrams.end()) {
+                vector<string> bucket = {str};
+                anagrams[sorted] = bucket;
+                continue;
+            }
+
+            // add to existing bucket
+           pos->second.push_back(str);
+        }
+
+        // add the groups back to result
+        for (auto it = anagrams.begin(); it != anagrams.end(); it++) {
+            result.push_back(it->second);
+        }
 
         return result;
     }
