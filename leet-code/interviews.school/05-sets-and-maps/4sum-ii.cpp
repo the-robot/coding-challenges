b20a76ee005aa30291804cfa72022b81563afb55
the-robot <9334746+the-robot@users.noreply.github.com>
2021-01-24 14:34:12 +0000

    added solution

--- leet-code/interviews.school/05-sets-and-maps/4sum-ii.cpp
+++ leet-code/interviews.school/05-sets-and-maps/4sum-ii.cpp
@@ -1,1 +1,33 @@
 // https://leetcode.com/problems/4sum-ii/
+
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+        if (A.size() == 0) {
+            return 0;
+        }
+        
+        unordered_map<int, int> m;
+        int count = 0;
+        
+        for (auto i : C) {
+            for (auto j : D) {
+                m[i+j]++;
+            }
+        }
+        
+        for (auto i : A) {
+            for (auto j : B) {
+                int x = -1 * (i+j);
+                count += m[x];
+            }
+        }
+        
+        return count;
+    }
+};
