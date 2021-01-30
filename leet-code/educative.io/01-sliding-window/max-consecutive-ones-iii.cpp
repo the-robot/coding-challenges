05da859b768345351fe84c9c73ee195e28fb242a
the-robot <9334746+the-robot@users.noreply.github.com>
2021-01-30 09:08:59 +0000

    added solution

--- leet-code/educative.io/01-sliding-window/max-consecutive-ones-iii.cpp
+++ leet-code/educative.io/01-sliding-window/max-consecutive-ones-iii.cpp
@@ -1,1 +1,31 @@
 // https://leetcode.com/problems/max-consecutive-ones-iii/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int longestOnes(vector<int>& A, int K) {
+        int maxLength = 0;
+        int maxOnesCount = 0;
+        int left = 0;
+        
+        for (int right = 0; right < A.size(); right++) {
+            if (A[right] == 1) {
+                maxOnesCount++;
+            }
+            
+            if (right - left + 1 - maxOnesCount > K) {
+                if (A[left] == 1) {
+                    maxOnesCount--;
+                }
+                left++;
+            }
+            
+            maxLength = max(maxLength, right - left + 1);
+        }
+        
+        return maxLength;
+    }
+};
