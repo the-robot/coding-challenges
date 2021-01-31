15c50a77916b1540e876f6366f1e15cae8f95b7c
the-robot <9334746+the-robot@users.noreply.github.com>
2021-01-31 06:34:44 +0000

    feat: added more solutions

--- leet-code/educative.io/01-sliding-window/find-all-anagrams-in-a-string.cpp
+++ leet-code/educative.io/01-sliding-window/find-all-anagrams-in-a-string.cpp
@@ -1,1 +1,54 @@
 // https://leetcode.com/problems/find-all-anagrams-in-a-string/
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    public:
+        vector<int> findAnagrams(string s, string p) {
+            // hashmap to calculate frequencies of all the characters in the pattern
+            unordered_map<char, int> patternFreq;
+            for (auto c : p) {
+                patternFreq[c]++;
+            }
+
+            vector<int> resultIndices;
+            int left = 0, matched = 0;
+
+            for (int right = 0; right < s.length(); right++) {
+                char rightChar = s[right];
+
+                if (patternFreq.find(rightChar) != patternFreq.end()) {
+                    // decrement the frequency of the matched character
+                    patternFreq[rightChar]--;
+                    // character is completely matched if freq becomes 0
+                    if (patternFreq[rightChar] == 0) {
+                        matched++;
+                    }
+                }
+                
+                if (matched == (int)patternFreq.size()) {
+                    resultIndices.push_back(left);
+                }
+                
+                // shrink the window
+                if (right >= p.length() - 1) {
+                    char leftChar = s[left++];
+
+                    // if the character going out was part of the pattern, put it back in the frequency
+                    if (patternFreq.find(leftChar) != patternFreq.end()) {
+                        if (patternFreq[leftChar] == 0) {
+                            matched--;
+                        }
+                        // put the character back for matching
+                        patternFreq[leftChar]++;
+                    }
+                }
+            }
+            
+            return resultIndices;
+        }
+};
--- leet-code/educative.io/01-sliding-window/permutation-in-string.cpp
+++ leet-code/educative.io/01-sliding-window/permutation-in-string.cpp
@@ -1,2 +1,52 @@
 // https://leetcode.com/problems/permutation-in-string/
 
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+    public:
+        bool checkInclusion(string s1, string s2) {
+            // hashmap to calculate frequencies of all the characters in the pattern
+            unordered_map<char, int> patternFreq;
+            for (auto c : s1) {
+                patternFreq[c]++;
+            }
+            
+            int left = 0, matched = 0;
+            
+            for (int right = 0; right < s2.length(); right++) {
+                char rightChar = s2[right];
+                
+                if (patternFreq.find(rightChar) != patternFreq.end()) {
+                    // drecrement the frequency of the matched character
+                    patternFreq[rightChar]--;
+                    // character is completely matched if freq becomes 0
+                    if (patternFreq[rightChar] == 0) {
+                        matched++;
+                    }
+                }
+                
+                if (matched == (int)patternFreq.size()) {
+                    return true;
+                }
+                
+                // shrink the window
+                if (right >= s1.length() - 1) {
+                    char leftChar = s2[left++];
+                    
+                    // if the character going out was part of the pattern, put it back in the frequency
+                    if (patternFreq.find(leftChar) != patternFreq.end()) {
+                        if (patternFreq[leftChar] == 0) {
+                            matched--;  // before putting the character back, decrement the matched count
+                        }
+                        // put the character back for matching
+                        patternFreq[leftChar]++;
+                    } 
+                }
+            }
+            
+            return false;
+        }
+};
