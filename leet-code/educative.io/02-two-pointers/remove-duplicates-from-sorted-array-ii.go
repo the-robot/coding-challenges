344b8b9e88c75dbd3caf5881ea05df7b43207067
the-robot <9334746+the-robot@users.noreply.github.com>
2021-02-03 04:59:32 +0000

    feat: add solution

--- leet-code/educative.io/02-two-pointers/remove-duplicates-from-sorted-array-ii.go
+++ leet-code/educative.io/02-two-pointers/remove-duplicates-from-sorted-array-ii.go
@@ -1,1 +1,19 @@
 // https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+
+package main
+
+func removeDuplicates(nums []int) int {
+	if len(nums) <= 1 {
+		return 1
+	}
+
+	i := 0
+	for _, n := range nums {
+		if i < 2 || n > nums[i-2] {
+			nums[i] = n
+			i++
+		}
+	}
+
+	return i
+}
--- leet-code/educative.io/02-two-pointers/remove-duplicates-from-sorted-list.go
+++ leet-code/educative.io/02-two-pointers/remove-duplicates-from-sorted-list.go
@@ -1,1 +1,24 @@
 // https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+package main
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	current := head
+
+	for current != nil {
+		if current.Next != nil && current.Val == current.Next.Val {
+			current.Next = current.Next.Next
+			continue
+		}
+
+		current = current.Next
+	}
+
+	return head
+}
