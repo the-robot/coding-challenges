000993b1ba47eee2939b3d663d375ab48fa9a380
the-robot <9334746+the-robot@users.noreply.github.com>
2021-02-03 11:49:17 +0000

    feat: git add more solutions

--- leet-code/educative.io/02-two-pointers/squares-of-a-sorted-array.go
+++ leet-code/educative.io/02-two-pointers/squares-of-a-sorted-array.go
@@ -1,1 +1,27 @@
 // https://leetcode.com/problems/squares-of-a-sorted-array/
+
+package main
+
+func sortedSquares(nums []int) []int {
+	var left int = 0
+	var right int = len(nums) - 1
+	var squared = make([]int, len(nums))
+	var sqrPtr = len(nums) - 1 // to store where to put next value
+
+	for sqrPtr >= 0 {
+		lSquared := nums[left] * nums[left]
+		rSquared := nums[right] * nums[right]
+
+		if lSquared > rSquared {
+			squared[sqrPtr] = lSquared
+			left++
+		} else {
+			squared[sqrPtr] = rSquared
+			right--
+		}
+
+		sqrPtr--
+	}
+
+	return squared
+}
