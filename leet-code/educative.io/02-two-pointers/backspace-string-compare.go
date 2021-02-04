438c1bca04bae4232f5bec3c8f7759a3fe1bcb9d
the-robot <9334746+the-robot@users.noreply.github.com>
2021-02-04 19:22:11 +0000

    feat: added solution

--- leet-code/educative.io/02-two-pointers/backspace-string-compare.go
+++ leet-code/educative.io/02-two-pointers/backspace-string-compare.go
@@ -1,1 +1,46 @@
 // https://leetcode.com/problems/backspace-string-compare/
+
+package main
+
+func backspaceCompare(S string, T string) bool {
+	index1 := len(S) - 1
+	index2 := len(T) - 1
+
+	for index1 >= 0 || index2 >= 0 {
+		i1 := getNextValidCharacter(S, index1)
+		i2 := getNextValidCharacter(T, index2)
+
+		if i1 < 0 && i2 < 0 {
+			return true
+		}
+		if i1 < 0 || i2 < 0 {
+			return false
+		}
+		if S[i1] != T[i2] {
+			return false
+		}
+
+		index1 = i1 - 1
+		index2 = i2 - 1
+	}
+
+	return true
+}
+
+func getNextValidCharacter(str string, index int) int {
+	backspaceCount := 0
+
+	for index >= 0 {
+		if string(str[index]) == "#" { // found a backspace
+			backspaceCount++
+		} else if backspaceCount > 0 { // a non backspace character
+			backspaceCount--
+		} else {
+			break
+		}
+
+		index -= 1 // skip a backspace or a valid character
+	}
+
+	return index
+}
--- leet-code/educative.io/02-two-pointers/shortest-unsorted-continuous-subarray.go
+++ leet-code/educative.io/02-two-pointers/shortest-unsorted-continuous-subarray.go
@@ -1,1 +1,50 @@
 // https://leetcode.com/problems/shortest-unsorted-continuous-subarray/
+
+package main
+
+import "math"
+
+func findUnsortedSubarray(nums []int) int {
+	low := 0
+	high := len(nums) - 1
+
+	// find first out of order number from start
+	for low < len(nums)-1 && nums[low] <= nums[low+1] {
+		low++
+	}
+
+	// array is sorted, if low is equal to last index
+	if low == len(nums)-1 {
+		return 0
+	}
+
+	// find first out of order number from last
+	for high > 0 && nums[high] >= nums[high-1] {
+		high--
+	}
+
+	// find the maximum and minimum of the subarray
+	subarrayMax := math.MinInt32
+	subarrayMin := math.MaxInt32
+	for k := low; k <= high; k++ {
+		if nums[k] > subarrayMax {
+			subarrayMax = nums[k]
+		}
+
+		if nums[k] < subarrayMin {
+			subarrayMin = nums[k]
+		}
+	}
+
+	// extend the low
+	for low > 0 && nums[low-1] > subarrayMin {
+		low--
+	}
+
+	// extend the high
+	for high < len(nums)-1 && nums[high+1] < subarrayMax {
+		high++
+	}
+
+	return high - low + 1
+}
