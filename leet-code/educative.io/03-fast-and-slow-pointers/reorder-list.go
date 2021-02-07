3c9772263b72e25d80a2fd763a70ebe4c2102c36
the-robot <9334746+the-robot@users.noreply.github.com>
2021-02-07 15:08:31 +0000

    feat: added solution

--- leet-code/educative.io/03-fast-and-slow-pointers/reorder-list.go
+++ leet-code/educative.io/03-fast-and-slow-pointers/reorder-list.go
@@ -7,3 +7,52 @@ type ListNode struct {
 	Val  int
 	Next *ListNode
 }
+
+func reorderList(head *ListNode) {
+	if head == nil || head.Next == nil {
+		return
+	}
+
+	// find middle of the Linked List
+	slow := head
+	fast := head
+
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+
+	// reverse the mid-end I.e.
+	// 1 -> 2 -> 3 -> 4 will become
+	// 1 -> 2 -> 4 -> 3
+	headFirstHalf := head
+	headSecondHalf := reverse(slow)
+
+	for headFirstHalf != nil && headSecondHalf != nil {
+		temp := headFirstHalf.Next
+		headFirstHalf.Next = headSecondHalf
+		headFirstHalf = temp
+
+		temp = headSecondHalf.Next
+		headSecondHalf.Next = headFirstHalf
+		headSecondHalf = temp
+	}
+
+	// set the next of the last node to null
+	if headFirstHalf != nil {
+		headFirstHalf.Next = nil
+	}
+}
+
+func reverse(head *ListNode) *ListNode {
+	var prev *ListNode = nil
+
+	for head != nil {
+		next := head.Next
+		head.Next = prev
+		prev = head
+		head = next
+	}
+
+	return prev
+}
