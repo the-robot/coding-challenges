99d54e79f0c0375dc8a5b52af8c06594ff9cecf0
the-robot <9334746+the-robot@users.noreply.github.com>
2021-02-07 16:46:40 +0000

    feat: added solution

--- leet-code/educative.io/03-fast-and-slow-pointers/reorder-list.go
+++ leet-code/educative.io/03-fast-and-slow-pointers/reorder-list.go
@@ -22,12 +22,72 @@ func reorderList(head *ListNode) {
 		slow = slow.Next
 	}
 
-	// reverse the mid-end I.e.
-	// 1 -> 2 -> 3 -> 4 will become
-	// 1 -> 2 -> 4 -> 3
-	headFirstHalf := head
-	headSecondHalf := reverse(slow)
+	headFirstHalf := head           /* 1 -> 2 -> 3 -> 4 */
+	headSecondHalf := reverse(slow) /* reverse the mid-end, 4 -> 3 */
 
+	/*
+		First iteration
+			head -> {
+				1 -> 2
+				2 -> 3
+				3 -> 4
+				4 -> nil
+			}
+			reversed half -> {
+				4 -> 3
+				3 -> nil
+			}
+
+			headFirstHalf = 1
+			headSecondHalf = 4
+
+			temp = 2
+			headFirstHalf.Next = 4
+			headFirstHalf = 2
+
+			... 1 -> 4 -> 3 -> nil
+
+			temp = 3
+			headSecondHalf.Next = 2
+			headSecondHalf = 3
+
+			... 1 -> 4 -> 2 -> 3
+
+		Second iteration
+			head -> {
+				1 -> 4
+				4 -> 2
+				2 -> 3
+				3 -> nil
+			}
+			reversed half -> {
+				4 -> 3
+				3 -> nil
+			}
+
+			headFirstHalf = 2
+			headSecondHalf = 3
+
+			temp = nil
+			headFirstHalf.Next = 3
+			headFirstHalf = 3
+
+			... 1 -> 4 -> 2 -> 4 -> nil
+
+			temp = nil
+			headSecondHalf.Next = 3 (now 3 is pointing itself, headSecondHalf is already 3)
+			headSecondHalf = nil
+
+			... 1 -> 4 -> 2 -> 3 -> 3 ...... (3 is pointing itself)
+
+		> since headSecondHalf = nil, iteration stops
+
+		after iteration, headFirstHalf = 3
+		since it is not equal to nil, set it's next to nil
+		means we removed the pointer 3, pointing itself
+
+		so answer is 1 -> 4 -> 2 -> 3
+	*/
 	for headFirstHalf != nil && headSecondHalf != nil {
 		temp := headFirstHalf.Next
 		headFirstHalf.Next = headSecondHalf
