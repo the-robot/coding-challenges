2eba7afe3f7b4653ec85190f50885dbbdf20bd3a
the-robot <9334746+the-robot@users.noreply.github.com>
2021-02-15 13:34:20 +0000

    feat: added BFS problems

---
+++ leet-code/educative.io/07-tree-breadth-first-search/minimum-depth-of-binary-tree.cpp
@@ -0,0 +1,64 @@
+// https://leetcode.com/problems/minimum-depth-of-binary-tree/
+
+using namespace std;
+
+#include <queue>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+};
+
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        
+        // add the root into queue
+        queue<TreeNode *>queue;
+        queue.push(root);
+        int depth = 0;
+        
+        while (!queue.empty()) {
+            depth++;
+
+            int levelSize = queue.size();
+            
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode *currentNode = queue.front();
+                queue.pop();
+                
+                // check if this is a leaf node
+                if (currentNode->left == nullptr && currentNode->right == nullptr) {
+                    return depth;
+                }
+                
+                // insert the children of current node in the queue
+                if (currentNode->left != nullptr) {
+                    queue.push(currentNode->left);
+                }
+                if (currentNode->right != nullptr) {
+                    queue.push(currentNode->right);
+                }
+            }
+        }
+
+        // because we do not count root as depth
+        return depth;
+    }
+};
