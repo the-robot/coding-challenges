9dec7f81a3a4fb6901ea800b5f2c6136dc3e1cff
the-robot <9334746+the-robot@users.noreply.github.com>
2021-03-04 16:44:27 +0000

    feat: added two pointer problem

---
+++ leet-code/educative.io/02-two-pointers/number-range.py
@@ -0,0 +1,26 @@
+# https://www.educative.io/courses/grokking-the-coding-interview/R1B78K9oBEz
+
+from typing import List
+
+class Solution:
+    def findRange(self, nums: List[int], target: int):
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            if nums[left] < target:
+                left += 1
+            elif nums[right] > target:
+                right -= 1
+            else:
+                return [left, right]
+
+        return [-1, -1]
+
+
+if __name__ == "__main__":
+    s = Solution()
+
+    print(s.findRange([4, 6, 6, 6, 9], 6))
+    print(s.findRange([4, 3, 8, 10, 15], 10))
+    print(s.findRange([1, 3, 8, 10, 15], 12))
