41808d76d2c2e561fee2758c34e5f7085500b464
the-robot <9334746+the-robot@users.noreply.github.com>
2021-03-04 16:47:28 +0000

    feat: added two pointer problem

--- leet-code/educative.io/02-two-pointers/number-range.py
+++ leet-code/educative.io/02-two-pointers/number-range.py
@@ -3,7 +3,7 @@
 from typing import List
 
 class Solution:
-    def findRange(self, nums: List[int], target: int):
+    def findRange(self, nums: List[int], target: int) -> List[int]:
         left = 0
         right = len(nums) - 1
 
