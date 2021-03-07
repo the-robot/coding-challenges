4ac5e794f566f08814351d7884a77dcee1235296
the-robot <9334746+the-robot@users.noreply.github.com>
2021-03-07 08:38:41 +0000

    feat: added top k elements problems

--- leet-code/educative.io/13-top-k-elements/connect-ropes.py
+++ leet-code/educative.io/13-top-k-elements/connect-ropes.py
@@ -1,12 +1,26 @@
 # https://www.educative.io/courses/grokking-the-coding-interview/qVZmZJVxPY0
 
 from typing import List
-
+import heapq
 
 class Solution:
     def connect(self, ropeLengths: List[int]) -> int:
-        pass
+        heapq.heapify(ropeLengths) # change into minheap
+        cost = 0
 
+        while len(ropeLengths) > 1:
+            # pop first 2 numbers
+            a = heapq.heappop(ropeLengths)
+            b = heapq.heappop(ropeLengths)
+
+            # connect 2 ropes and add the cost
+            ropeSum = a + b
+            cost += ropeSum
+
+            # add back the connected rope
+            heapq.heappush(ropeLengths, ropeSum)
+
+        return cost
 
 if __name__ == "__main__":
     s = Solution()
