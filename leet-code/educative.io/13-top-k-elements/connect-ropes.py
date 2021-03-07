864ea42e3578ab9352079e53e3373c8fd44a7a6a
the-robot <9334746+the-robot@users.noreply.github.com>
2021-03-07 08:31:28 +0000

    init: top k elements problem

---
+++ leet-code/educative.io/13-top-k-elements/connect-ropes.py
@@ -0,0 +1,16 @@
+# https://www.educative.io/courses/grokking-the-coding-interview/qVZmZJVxPY0
+
+from typing import List
+
+
+class Solution:
+    def connect(self, ropeLengths: List[int]) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+
+    print(s.connect([1, 3, 11, 5]))
+    print(s.connect([3, 4, 5, 6]))
+    print(s.connect([1, 3, 11, 5, 2]))
--- leet-code/educative.io/13-top-k-elements/kth-largest-element-in-an-array.py
+++ leet-code/educative.io/13-top-k-elements/kth-largest-element-in-an-array.py
@@ -4,31 +4,17 @@ from typing import List
 import heapq
 
 class Solution:
-    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        """
-        Calculate euclidean distance and store in * -1, so that we can reverse the heap.
-        Python default heap is minheap so by multiplying with -1 we store as maxheap.
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heapq._heapify_max(nums)
+        largestKth = None
         
-        We only store k minimum distances and pop any extras.
-        """
-        maxheap = [] # store {distance, point} pair in maxheap
-        result = []
+        i = 0
+        while len(nums) > 0 and i < k:
+            # if the popped number is kth index, it is the largest
+            popped = heapq._heappop_max(nums)
+            if i == k - 1:
+                largestKth = popped
 
-        # calculate distances and add into maxheap
-        for point in points:
-            # correct euclidean distance formula is sqrt((x2 - x1)^2 + (y2 - y1)^2)
-            # in this case, no point sqrt since we do not really care about actual distance.
-            # also * -1 so that we pop largest distances first.
-            distance = ((point[0] * point[0]) + (point[1] * point[1])) * -1
-            heapq.heappush(maxheap, [distance, point])
-
-            # pop any distances larger than k
-            if len(maxheap) > k:
-                heapq.heappop(maxheap)
-
-        # get first k closest points
-        while len(maxheap) > 0:
-            popped = heapq.heappop(maxheap)
-            result.append(popped[1])
-
-        return result
+            i += 1
+    
+        return largestKth
