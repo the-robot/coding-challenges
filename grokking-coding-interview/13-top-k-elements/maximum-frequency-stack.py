06ca3f2dec576f8d7a9000b1b4554a5388983ca4
the-robot <9334746+the-robot@users.noreply.github.com>
2021-03-07 17:51:44 +0000

    feat: added top k elements problems

--- grokking-coding-interview/13-top-k-elements/maximum-frequency-stack.py
+++ grokking-coding-interview/13-top-k-elements/maximum-frequency-stack.py
@@ -1,2 +1,53 @@
 # https://leetcode.com/problems/maximum-frequency-stack/
 
+import heapq
+
+
+class Element:
+    def __init__(self, number: int, frequency: int, sequenceNumber: int):
+        # value of the number
+        self.number = number
+        
+        # current frequency of the number when it was pushed to the top
+        self.frequency = frequency
+        
+        # to konw what number came first
+        self.sequenceNumber = sequenceNumber
+    
+    def __lt__(self, other: 'Element') -> bool:
+        # higher frequency wins
+        if self.frequency != other.frequency:
+            return self.frequency > other.frequency
+    
+        # if both element have same frequency, return the element that was pushed later
+        return self.sequenceNumber > other.sequenceNumber
+        
+
+class FreqStack:
+    """Max Heap"""
+
+    def __init__(self):
+        self.sequenceNumber = 0
+        self.maxHeap = []
+        self.frequencyMap = {}
+
+    def push(self, val: int) -> None:
+        self.frequencyMap[val] = self.frequencyMap.get(val, 0) + 1
+        
+        heapq.heappush(self.maxHeap, Element(
+            val, self.frequencyMap[val], self.sequenceNumber,
+        ))
+        
+        # increment sequence number
+        self.sequenceNumber += 1
+
+    def pop(self) -> int:
+        val = heapq.heappop(self.maxHeap).number
+        
+        # decrement the frequency or remove if this is the last number
+        if self.frequencyMap[val] > 1:
+            self.frequencyMap[val] -= 1
+        else:
+            del self.frequencyMap[val]
+        
+        return val
