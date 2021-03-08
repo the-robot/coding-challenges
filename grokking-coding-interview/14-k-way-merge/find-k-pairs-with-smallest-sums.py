9fc0d073daad189db68b958929592f218cc0f606
the-robot <9334746+the-robot@users.noreply.github.com>
2021-03-08 18:42:34 +0000

    feat: added K-way merge problems

--- grokking-coding-interview/14-k-way-merge/find-k-pairs-with-smallest-sums.py
+++ grokking-coding-interview/14-k-way-merge/find-k-pairs-with-smallest-sums.py
@@ -19,15 +19,26 @@ class Solution:
             for j in range(0, min(k, len(nums2))):
                 x = nums1[i]
                 y = nums2[j]
-                
-                total = x + y
-                heapq.heappush(maxHeap, [-total, x, y])
 
-                # pop the largest number among k+1 numbers in maxHeap, so that only
-				# k smallest numbers are in maxHeap
-                if len(maxHeap) > k:
+                # sum of two number
+                total = x + y
+
+                if len(maxHeap) < k:
+                    heapq.heappush(maxHeap, [-total, x, y])
+                else:
+                    # if the sum of x and y is larger than the largest (among the k smallests)
+                    # sum, we can 'break' here. Since the arrays are sorted in the ascending order,
+                    # we will not be able to find a pair with smaller sum moving forward.
+                    if total > -maxHeap[0][0]:
+                        break
+
+                    # push new numbers to the heap
+                    heapq.heappush(maxHeap, [-total, x, y])
+
+                    # pop the largest number among k+1 numbers in maxHeap, so that only
+                    # k smallest numbers are in maxHeap
                     heapq.heappop(maxHeap)
-                    
+
         result = []
         while maxHeap:
             popped = heapq.heappop(maxHeap)
