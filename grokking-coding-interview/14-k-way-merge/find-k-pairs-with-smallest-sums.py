d0f4ba3196a45f4273601556bb1a3f3263669bd1
the-robot <9334746+the-robot@users.noreply.github.com>
2021-03-08 18:19:25 +0000

    feat: added K-way merge problems

--- grokking-coding-interview/14-k-way-merge/find-k-pairs-with-smallest-sums.py
+++ grokking-coding-interview/14-k-way-merge/find-k-pairs-with-smallest-sums.py
@@ -2,3 +2,25 @@
 
 from typing import List
 import heapq
+
+class Solution:
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        # we use maxHeap so that we can pop largest number among k+1 numbers in maxHeap first
+        maxHeap = []
+
+        for x in nums1:
+            for y in nums2:
+                total = x + y
+                heapq.heappush(maxHeap, [-total, [x, y]])
+
+                # pop the largest number among k+1 numbers in maxHeap, so that only
+				# k smallest numbers are in maxHeap
+                if len(maxHeap) > k:
+                    heapq.heappop(maxHeap)
+
+        result = []
+        while maxHeap:
+            popped = heapq.heappop(maxHeap)
+            result.append(popped[1])
+
+        return result
