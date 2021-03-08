c802520706e743c04eafc9a12e985fe7107bea97
the-robot <9334746+the-robot@users.noreply.github.com>
2021-03-08 18:32:15 +0000

    init: new topic dynamic programming

--- grokking-coding-interview/14-k-way-merge/find-k-pairs-with-smallest-sums.py
+++ grokking-coding-interview/14-k-way-merge/find-k-pairs-with-smallest-sums.py
@@ -8,19 +8,29 @@ class Solution:
         # we use maxHeap so that we can pop largest number among k+1 numbers in maxHeap first
         maxHeap = []
 
-        for x in nums1:
-            for y in nums2:
+        """
+        instead of iterating over all the numbers of both array, we can iterate only
+        the first 'K' numbers from both array.
+
+        Since they are sorted in ascending order, the pairs with the minimum sum will
+        be just the first 'K' numbers from those two arrays.
+        """
+        for i in range(0, min(k, len(nums1))):
+            for j in range(0, min(k, len(nums2))):
+                x = nums1[i]
+                y = nums2[j]
+                
                 total = x + y
-                heapq.heappush(maxHeap, [-total, [x, y]])
+                heapq.heappush(maxHeap, [-total, x, y])
 
                 # pop the largest number among k+1 numbers in maxHeap, so that only
 				# k smallest numbers are in maxHeap
                 if len(maxHeap) > k:
                     heapq.heappop(maxHeap)
-
+                    
         result = []
         while maxHeap:
             popped = heapq.heappop(maxHeap)
-            result.append(popped[1])
+            result.append([popped[1], popped[2]])
 
         return result
