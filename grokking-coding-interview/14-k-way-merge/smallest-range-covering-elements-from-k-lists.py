e16e893396afcba6d1fe6e2216881b1eec50e417
the-robot <9334746+the-robot@users.noreply.github.com>
2021-03-08 17:31:21 +0000

    feat: added K-way merge problems

---
+++ grokking-coding-interview/14-k-way-merge/smallest-range-covering-elements-from-k-lists.py
@@ -0,0 +1,38 @@
+# https://leetcode.com/problems/smallest-range-covering-elements-from-k-lists/
+
+from typing import List
+import heapq
+
+class Solution:
+    def smallestRange(self, nums: List[List[int]]) -> List[int]:
+        minHeap = []
+        rangeStart = 0
+        rangeEnd = math.inf
+
+        # max number among first number of each arr
+        currentMaxNumber = -math.inf 
+
+        # put the 1st element of each array in the max heap
+        for arr in nums:
+            heappush(minHeap, (arr[0], 0, arr))
+            currentMaxNumber = max(currentMaxNumber, arr[0])
+
+        """
+        take the smallest (top) element from the min heap
+        - if it gives us smaller range, update the ranges
+        - if the array of the top element has more elements,
+          insert the next element in the heap
+        """
+        while len(minHeap) == len(nums):
+            num, i, arr = heapq.heappop(minHeap)
+
+            if rangeEnd - rangeStart > currentMaxNumber - num:
+                rangeStart = num
+                rangeEnd = currentMaxNumber
+
+            if len(arr) > i + 1:
+                # insert the next element in the heap
+                heapq.heappush(minHeap, (arr[i+1], i+1, arr))
+                currentMaxNumber = max(currentMaxNumber, arr[i+1])
+
+        return [rangeStart, rangeEnd]
