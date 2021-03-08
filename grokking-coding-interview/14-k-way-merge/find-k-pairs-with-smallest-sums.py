0490ff219b3a84fe3ab3c5faa02843062596fc8b
the-robot <9334746+the-robot@users.noreply.github.com>
2021-03-08 18:12:49 +0000

    feat: added K-way merge problems

---
+++ grokking-coding-interview/14-k-way-merge/find-k-pairs-with-smallest-sums.py
@@ -0,0 +1,4 @@
+# https://leetcode.com/problems/find-k-pairs-with-smallest-sums/
+
+from typing import List
+import heapq
--- grokking-coding-interview/14-k-way-merge/smallest-range-covering-elements-from-k-lists.py
+++ grokking-coding-interview/14-k-way-merge/smallest-range-covering-elements-from-k-lists.py
@@ -6,9 +6,21 @@ import math
 
 class Solution:
     def smallestRange(self, nums: List[List[int]]) -> List[int]:
-        minHeap = []
-        rangeStart = 0
-        rangeEnd = math.inf
+        """
+        First you put all into minHeap, so numbers are sorted from
+        smallest first number -> largest first number.
+
+        I.e. [[4,10,15,24,26],[0,9,12,20],[5,18,22,30]] -> [[0,9,12,20],[4,10,15,24,26],[5,18,22,30]]
+
+        In the meantime, you also need to store the largest among the first of each 3 nums. The reason
+        is from 0 (start as default) to that max (5 in this case), is a range (0-5) that includes at least
+        one number from each of the k lists.
+
+        From that, what we need to do is, increment the `start` while decrementing the `end` and get a smallest
+        range that includes at least number one from each k list.
+        """
+
+        minHeap = [] # {first number of array, index, [int]}
 
         # max number among first number of each arr
         currentMaxNumber = -math.inf 
@@ -18,15 +30,25 @@ class Solution:
             heapq.heappush(minHeap, (arr[0], 0, arr))
             currentMaxNumber = max(currentMaxNumber, arr[0])
 
+        # Smallest range that includes at least one number from each
+        # of the k lists. Default is 0 to inf.
+        rangeStart = 0
+        rangeEnd = math.inf
+
         """
         take the smallest (top) element from the min heap
         - if it gives us smaller range, update the ranges
         - if the array of the top element has more elements,
           insert the next element in the heap
+
+        currentMaxNumber here is to keep track of next max number.
+        Because in every iteration, the start will rangeStart increasing. Therefore,
+        we also need to expand the rangeEnd so that overall range still covers all k lists.
         """
         while len(minHeap) == len(nums):
             num, i, arr = heapq.heappop(minHeap)
 
+            # if we find a smaller range, replace that to range start-end
             if rangeEnd - rangeStart > currentMaxNumber - num:
                 rangeStart = num
                 rangeEnd = currentMaxNumber
