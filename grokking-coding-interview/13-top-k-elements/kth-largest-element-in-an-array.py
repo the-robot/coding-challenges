57b085b744eac01e817971b43631bc2c85c81173
the-robot <9334746+the-robot@users.noreply.github.com>
2021-05-09 01:38:06 +0800

    Update kth-largest-element-in-an-array.py

--- grokking-coding-interview/13-top-k-elements/kth-largest-element-in-an-array.py
+++ grokking-coding-interview/13-top-k-elements/kth-largest-element-in-an-array.py
@@ -5,6 +5,23 @@ import heapq
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        minHeap = []
+        for num in nums:
+            heapq.heappush(minHeap, num)
+            
+            # if greater than k numbers
+            # pop the smallest from min heap
+            if len(minHeap) > k:
+                heapq.heappop(minHeap)
+        
+        # kth largest element would be first index
+        # in min heap of k largest numbers
+        return minHeap[0]
+
+
+class Solution2:
+    """ Not recommended as it uses private methods """
+    def findKthLargest(self, nums: List[int], k: int) -> int:
         heapq._heapify_max(nums)
         largestKth = None
         
