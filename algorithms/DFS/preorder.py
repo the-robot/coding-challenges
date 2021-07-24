a41b5068dcb60a32799c6e91c57b50c4b40bfdbb
the-robot <9334746+the-robot@users.noreply.github.com>
2021-07-25 01:54:42 +0800

    Update preorder.py

--- algorithms/DFS/preorder.py
+++ algorithms/DFS/preorder.py
@@ -1,4 +1,9 @@
-from tree import TreeNode
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 def preorder(root: TreeNode):
     stack = [root]
