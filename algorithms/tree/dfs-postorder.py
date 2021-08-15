47b2132bd014a3a95d8f174c4e7d551611e77768
the-robot <9334746+the-robot@users.noreply.github.com>
2021-08-15 15:35:34 +0800

    Update dfs-postorder.py

--- algorithms/tree/dfs-postorder.py
+++ algorithms/tree/dfs-postorder.py
@@ -5,6 +5,7 @@ class TreeNode:
         self.left = left
         self.right = right
 
+
 def postorder(root: TreeNode):
     if root == None:
         return
@@ -17,3 +18,29 @@ def postorder(root: TreeNode):
     
     # visit root
     print(root.val, end = " ")
+
+
+def postorder_iterative(root: TreeNode):
+    stack = [root]
+    output = []
+        
+    while stack:
+        current = stack.pop()
+
+        output.append(current.val)
+
+        # Add left first, because in stack it is LIFO (Last In First Out)
+        # because for pre-order, we want right first, so we add it later
+
+        if current.left:
+            stack.append(current.left)
+
+        if current.right:
+            stack.append(current.right)
+
+    # reverse it
+    output.reverse()
+    
+    # print values
+    for v in reversed(output):
+        print(v, end = " ")
