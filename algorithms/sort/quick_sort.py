fcab45e1938944c62df4b6d7da5f5653a5a5d56c
the-robot <9334746+the-robot@users.noreply.github.com>
2021-08-23 15:34:33 +0800

    Update quick_sort.py

--- algorithms/sort/quick_sort.py
+++ algorithms/sort/quick_sort.py
@@ -1,34 +1,34 @@
 def merge_sort(array):
-	n = len(array)
+    n = len(array)
 	
-	if n <= 1:
-		return array
+    if n <= 1:
+        return array
 
-	middle = n // 2
+    middle = n // 2
 
-	left = merge_sort(array[0 : middle])
-	right = merge_sort(array[middle : n])
+    left = merge_sort(array[0 : middle])
+    right = merge_sort(array[middle : n])
 
-	return merge(left, right)
+    return merge(left, right)
+
 
 def merge(a, b):
-	array = []
+    array = []
     
-	while a and b:
-		if a[0] > b[0]:
-			array.append(b[0])
-			del b[0]
-        
-		else:
-			array.append(a[0])
-			del a[0]
+    while a and b:
+        if a[0] > b[0]:
+            array.append(b[0])
+            del b[0]
+        else:
+            array.append(a[0])
+            del a[0]
     
-	while a:
-		array.append(a[0])
-		del a[0]
+    while a:
+        array.append(a[0])
+        del a[0]
     		
-	while b:
-		array.append(b[0])
-		del b[0]
+    while b:
+        array.append(b[0])
+        del b[0]
     
-	return array
+    return array
